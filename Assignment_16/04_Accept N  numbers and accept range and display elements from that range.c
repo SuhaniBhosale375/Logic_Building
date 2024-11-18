2770567b0d3c63afe0bf0821827d5647de09ae11
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-11-18 10:48:03 +0530

    C Programming

---
+++ Assignment_16/04_Accept N  numbers and accept range and display elements from that range.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Range(int Arr[],int iLength,int iStart, int iEnd)
+{
+    int i = 0,iCnt = 0;
+
+   for(i = 0; i < iLength; i++)
+    {
+        if((Arr[i] < iEnd)  &&  (Arr[i] > iStart) )
+        {
+            printf("%d\t",Arr[i]);
+        }
+    }
+}
+int main()
+{
+    int iSize = 0 ,iCnt = 0,iValue1 = 0,iValue2 = 0;
+    int*p = NULL;
+     
+    printf("Enter number of elments : ");
+    scanf("%d",&iSize);
+
+    printf("Enter the starting point:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the ending point:\n");
+    scanf("%d",&iValue2);
+
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element : %d\n",iCnt+1);
+        
+        scanf("%d",&p[iCnt]);
+    }
+   
+    Range(p,iSize,iValue1,iValue2);
+
+    free(p);
+    
+    return 0;
+}
