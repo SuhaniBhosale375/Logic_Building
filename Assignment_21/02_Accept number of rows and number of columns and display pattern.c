bb5e0e934c876578ba613ca4198de1f357693cf5
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-11-24 10:09:02 +0530

    C Programming

---
+++ Assignment_21/02_Accept number of rows and number of columns and display pattern.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    int iCnt = 0;
+
+    for(i = 1; i<= iRow; i++)
+    {
+        for(j = 1; j<=iCol; j++,iCnt++)
+        {
+            if((i % 2) == 0)
+            {
+                printf("%d\t",iCnt * 2);
+            }
+            else
+            {
+                printf("%d\t",iCnt);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of Colums");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2 );
+
+    return 0;
+}
