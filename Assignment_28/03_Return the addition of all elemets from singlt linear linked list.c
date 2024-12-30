57a11b47a5524b61d3438cadcb08899874b793f8
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-12-30 08:31:55 +0530

    C Programming

---
+++ Assignment_28/03_Return the addition of all elemets from singlt linear linked list.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertLast(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+    PNODE temp = *First;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL)  
+    {
+        *First = newn;
+    }
+    else                
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+}
+
+void InsertFirst(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL) 
+    {
+        *First = newn;
+    }
+    else                
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}
+
+void Display(PNODE First)
+{
+    printf("Elements from the Linked List are : \n");
+
+    while(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        First = First -> next;
+    }
+    printf("NULL \n");
+}
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+int Addition(PNODE First)
+{
+    int iSum = 0;
+    
+    while(First != NULL)
+    {
+        iSum = iSum + First->data;
+        First = First->next;
+    }
+    return iSum;
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    int iValue = 0;
+
+    InsertFirst(&Head,40);
+    InsertFirst(&Head,30);
+    InsertFirst(&Head,20);
+    InsertFirst(&Head,10);
+    Display(Head);
+
+    iRet = Addition(Head);
+    printf("Addition of linked list element is : %d\n",iRet);
+
+    return 0;
+}
