66ae5b1d169df25ee0027c2b56692b66091bd351
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-12-31 06:51:39 +0530

    C Programming

---
+++ Assignment_28/04_return Largest element from singly linear linked list.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertLast(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+    PNODE temp = *First;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL)  
+    {
+        *First = newn;
+    }
+    else                
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+}
+
+void InsertFirst(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL) 
+    {
+        *First = newn;
+    }
+    else                
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}
+
+void Display(PNODE First)
+{
+    printf("Elements from the Linked List are : \n");
+
+    while(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        First = First -> next;
+    }
+    printf("NULL \n");
+}
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+int Maximum(PNODE First)
+{
+    int iMax = 0;
+
+    iMax = First->data;
+    
+    while(First != NULL)
+    {
+        if(First->data > iMax)
+        {
+            iMax = First->data;
+        }
+        First = First->next;
+    }
+    return iMax;
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    int iValue = 0;
+
+    InsertLast(&Head,110);
+    InsertLast(&Head,230);
+    InsertLast(&Head,320);
+    InsertLast(&Head,240);
+
+    Display(Head);
+
+    iRet = Maximum(Head);
+    printf("Largest element is : %d\n",iRet);
+
+    return 0;
+}
