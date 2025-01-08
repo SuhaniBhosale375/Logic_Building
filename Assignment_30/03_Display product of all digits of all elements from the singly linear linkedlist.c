ec9ea293690930bb4a283bb88c652f34659b77e8
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-01-08 18:50:46 +0530

    C Programming

---
+++ Assignment_30/03_Display product of all digits of all elements from the singly linear linkedlist.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertLast(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+    PNODE temp = *First;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL)  
+    {
+        *First = newn;
+    }
+    else                
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+}
+
+void InsertFirst(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL) 
+    {
+        *First = newn;
+    }
+    else                
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}
+
+void Display(PNODE First)
+{
+    printf("Elements from the Linked List are : \n");
+
+    while(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        First = First -> next;
+    }
+    printf("NULL \n");
+}
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+void DisplayProduct(PNODE First)
+{
+    printf("Product of each elements in Linked List :\n");
+    while(First != NULL)
+    {
+        int iDig = 0,iTemp = 0;
+        int iMult = 1;
+
+        iTemp = First ->data;
+        
+        while(iTemp != 0)
+        {
+            iDig= iTemp % 10;
+            iMult = iMult * iDig;
+            iTemp = iTemp/10;
+        }
+        printf("%d\t",iMult);
+        First =  First->next;
+    }
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    int iValue = 0;
+
+    InsertLast(&Head,11);
+    InsertLast(&Head,28);
+    InsertLast(&Head,17);
+    InsertLast(&Head,414);
+    InsertLast(&Head,6);
+    InsertLast(&Head,89);
+
+    Display(Head);
+
+    DisplayProduct(Head);
+   
+    return 0;
+}
