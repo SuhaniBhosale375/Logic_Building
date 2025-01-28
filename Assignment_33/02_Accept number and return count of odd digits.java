a9e9891f9c19358f71a0c1bd3b2926c2fc04897b
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-01-28 09:56:52 +0530

    Java Programming

---
+++ Assignment_33/02_Accept number and return count of odd digits.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Prog33_1
+{
+    public static void main(String Arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter any number :");
+        int Value = sobj.nextInt();
+
+        Digit obj = new Digit();
+
+        int iRet = obj.CountOdd(Value);
+
+        System.out.println("Count of odd digit is : "+iRet);
+    }
+}
+
+class Digit
+{
+    public int CountOdd(int iNo)
+    {
+        int iDigit = 0,iCnt = 0;
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+
+            if((iDigit % 2) != 0)
+            {
+                iCnt++;
+            }
+            iNo = iNo / 10;
+        }
+        return iCnt++;
+    }
+}
