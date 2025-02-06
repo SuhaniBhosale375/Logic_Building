86427733b5aff42eee56a0ed2d6f2446d8f8c6bf
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-02-06 21:39:40 +0530

    Java Programs

---
+++ Assignment_34/02_Accept N numbers and return the first occurence in the array.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Program261
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the size of array that we want to create :");
+        int iSize = sobj.nextInt();
+
+        System.out.println("Enter the number that we want to check first occurence: :");
+        int No = sobj.nextInt();
+
+        Number obj = new Number(iSize);
+        
+        obj.Accept();
+        obj.Display();
+
+        int index = obj.FirstOcuurence(No);
+
+        System.out.println("First occurence is : "+index);
+    }
+}
+class Number
+{
+    public int Arr[];
+    
+    public Number(int iSize)
+    {
+        Arr = new int[iSize];
+    }
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Please enter "+Arr.length+" elements");
+
+        for(int iCnt = 0; iCnt < Arr.length;iCnt++)
+        {
+            System.out.println("Element no :"+(iCnt+1));
+            Arr[iCnt] = sobj.nextInt();
+        }
+
+    }
+    public void Display()
+    {
+        System.out.println("Elements of the Array are :");
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.out.print(Arr[iCnt]+"\t");
+        }
+        System.out.println();
+    }
+
+    public int FirstOcuurence(int No)
+    {
+        int index = Arr[0];
+        for(int iCnt = 0; iCnt < Arr.length;iCnt++)
+        {
+            if(Arr[iCnt] == No)
+            {
+                index = iCnt;
+                break;
+            }
+            
+        }
+        return index;
+
+    }
+}
