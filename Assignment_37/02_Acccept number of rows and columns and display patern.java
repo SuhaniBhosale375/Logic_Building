034c84de4670ddec8fabb264e894307883439cc4
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-03-11 00:08:16 +0530

    Java Programms

---
+++ Assignment_37/02_Acccept number of rows and columns and display patern.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Prog36_1
+{
+    public static void main(String arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of rows :");
+        int iRow = sobj.nextInt();
+
+        System.out.println("Enter number of colomns:");
+        int iCol = sobj.nextInt();
+
+        Pattern obj = new Pattern();
+
+        obj.Display(iRow,iCol);
+    }
+}
+
+class Pattern
+{
+    public void Display(int Row,int Col)
+    {
+        for(int i = 1; i <= Row; i++)
+        {
+            for(int j = 1; j <= Col; j++)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
