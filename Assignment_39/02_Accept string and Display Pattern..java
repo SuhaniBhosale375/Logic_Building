e6ad4e8f1ed972e0221eacc9982a7ae441e8d93e
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-03-15 22:14:26 +0530

    Java Programms

---
+++ Assignment_39/02_Accept string and Display Pattern..java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Prog38_5
+{
+    public static void main(String arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String :");
+        String s = sobj.nextLine();
+
+        Pattern obj = new Pattern();
+
+        obj.Display(s);
+    }
+}
+
+class Pattern
+{
+    public void Display(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        for(int i = Arr.length; i > 0 ; i--)
+        {
+            for(int j = 0; j < Arr.length; j++)
+            {
+                if((i > j))
+                {
+                    System.out.print(Arr[j]+"\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
